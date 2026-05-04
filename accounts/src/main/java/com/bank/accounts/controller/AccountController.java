bf40cdcb587c02ebbf083ced19e971194de14303
Hari Mhetre <hari.mhetre2222@gmail.com>
2026-05-04 19:29:19 +0530

    Added circuit braker pattern

--- accounts/src/main/java/com/bank/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/bank/accounts/controller/AccountController.java
@@ -24,6 +24,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 //@AllArgsConstructor
